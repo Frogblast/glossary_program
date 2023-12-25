a7ad8ed6ba05aca0d131aa4292dde4a950379044
Frogblast <89352653+Frogblast@users.noreply.github.com>
2023-12-25 22:07:56 +0100

    added a simple unordered map (hashtable)

--- main.cpp
+++ main.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-int main(){
+int main()
+{
+
+    const int mapSize = 2;
     // Create hashmap with words
     // TODO: Read from a text-file
+    // Create an unordered_map with int keys and string values
+    unordered_map<string, string> glossaryMap;
+
+    glossaryMap["Czesc"] = "Hej";
+    glossaryMap["Slowo"] = "Ord";
+
+
+    std::cout << "HashMap contents:" << std::endl;
+    for (const auto &pair : glossaryMap)
+    {
+        cout << "Key: " << pair.first << " Value: " << pair.second << endl;
+    }
 
     // Show next word to be translated
-    
+
     // Get input
 
     // If correct (some % of correct letters) then congratulate, if not show the correct translation.
 
     // Go back to showing of the next word.
-
 
     return 0;
 }
