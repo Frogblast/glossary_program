6157cf9f59591f60cc27cb41222a1cef9cc58a28
Frogblast <89352653+Frogblast@users.noreply.github.com>
2023-12-25 21:44:03 +0100

    Set up project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Create hashmap with words
+    // TODO: Read from a text-file
+
+    // Show next word to be translated
+    
+    // Get input
+
+    // If correct (some % of correct letters) then congratulate, if not show the correct translation.
+
+    // Go back to showing of the next word.
+
+
+    return 0;
+}
